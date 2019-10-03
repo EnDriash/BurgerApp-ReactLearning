052f4f9ffaec7f8ad1e6c61c47391b5726223d4c
Jędrzej Krawczyk <siewierjedr@gmail.com>
2019-10-03 13:58:37 +0200

    Updates

---
+++ precache-manifest.abf6cfcda067b8228af7850d2bbbc387.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5c2157471687ae9fef8c44a032619c4",
+    "url": "/BurgerApp-ReactLearning/index.html"
+  },
+  {
+    "revision": "0bfd09ba027b1a2548b7",
+    "url": "/BurgerApp-ReactLearning/static/css/main.c609f28b.chunk.css"
+  },
+  {
+    "revision": "62213c9cff74a386d653",
+    "url": "/BurgerApp-ReactLearning/static/js/2.f506f34f.chunk.js"
+  },
+  {
+    "revision": "0bfd09ba027b1a2548b7",
+    "url": "/BurgerApp-ReactLearning/static/js/main.c98fe361.chunk.js"
+  },
+  {
+    "revision": "4be0773abb16a5dc93cd",
+    "url": "/BurgerApp-ReactLearning/static/js/runtime-main.1d8a8376.js"
+  }
+]);
